4322b751fbf06c3ff009271293e884280855dee7
Arutyunyan A. <casnerano@mail.ru>
2026-01-07 12:53:25 +0300

    Fix style

--- pkg/guard/guard.go
+++ pkg/guard/guard.go
@@ -3,43 +3,43 @@ package guard
 type Match int
 
 const (
-    MatchAtLeastOne Match = iota
-    MatchAll
+	MatchAtLeastOne Match = iota
+	MatchAll
 )
 
 type Rule struct {
-    AllowPublic           *bool
-    RequireAuthentication *bool
-    AuthenticatedAccess   *AuthenticatedAccess
+	AllowPublic           *bool
+	RequireAuthentication *bool
+	AuthenticatedAccess   *AuthenticatedAccess
 }
 
 type Rules []*Rule
 
 type AuthenticatedAccess struct {
-    RoleBased   *RoleBased
-    PolicyBased *PolicyBased
+	RoleBased   *RoleBased
+	PolicyBased *PolicyBased
 }
 
 type RoleBased struct {
-    Roles []string
-    Match Match
+	Roles []string
+	Match Match
 }
 
 type PolicyBased struct {
-    Policies []string
-    Match    Match
+	Policies []string
+	Match    Match
 }
 
 type Service struct {
-    Name    string
-    Rules   Rules
-    Methods map[string]*Method
+	Name    string
+	Rules   Rules
+	Methods map[string]*Method
 }
 
 type Method struct {
-    Rules Rules
+	Rules Rules
 }
 
 func Ptr[T any](v T) *T {
-    return &v
+	return &v
 }
