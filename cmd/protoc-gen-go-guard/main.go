5590b3f2de8971abfcfd0ea0ec5557281c317c6a
Arutyunyan A. <46101935+casnerano@users.noreply.github.com>
2026-02-08 15:18:45 +0300

    Refactoring (#17)
    * Rename meta attr
    
    * Inject build versions

--- cmd/protoc-gen-go-guard/main.go
+++ cmd/protoc-gen-go-guard/main.go
@@ -1,15 +1,45 @@
 package main
 
 import (
+	"fmt"
+	"runtime/debug"
+
 	guardplugin "github.com/casnerano/protoc-gen-go-guard/internal/plugin"
 	"google.golang.org/protobuf/compiler/protogen"
 	"google.golang.org/protobuf/types/pluginpb"
 )
 
+var version string
+
 func main() {
 	protogen.Options{}.Run(func(plugin *protogen.Plugin) error {
 		plugin.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
 
-		return guardplugin.Execute(plugin)
+		meta := guardplugin.Meta{
+			ProtocVersion: getProtocVersion(plugin),
+			PluginVersion: getPluginVersion(),
+		}
+
+		return guardplugin.Execute(plugin, meta)
 	})
+}
+
+func getPluginVersion() string {
+	if version != "" {
+		return version
+	}
+
+	if info, ok := debug.ReadBuildInfo(); ok {
+		return info.Main.Version
+	}
+
+	return "(devel)"
+}
+
+func getProtocVersion(plugin *protogen.Plugin) string {
+	if ver := plugin.Request.CompilerVersion; ver != nil {
+		return fmt.Sprintf("v%d.%d.%d", *ver.Major, *ver.Minor, *ver.Patch)
+	}
+
+	return "(unknown)"
 }
